f7581a987b9899eaccfe6086a80f1595d988e8df
Mishael Kama <mishealkama@gmail.com>
2018-01-08 00:25:29 +0100

    New commit
    Finished the task

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/something">
+
+
+
+
+ 
+</head>
+<body>
+    
+    <header>
+        <i class="material-icons menuBtn">menu</i>
+        <img src="img/retro.png" alt="">
+        <img src="img/logo.png" class="marginImg" alt="">
+
+        <ul>    
+          <li class="msg"><i class="material-icons">email</i></li>
+          <li class="noty"><i class="material-icons">notifications_none</i><span>3</span></li>
+          <li class="user"><i class="material-icons">person</i><h6>Welcome <b>Samuel</b></h6> <i class="material-icons">arrow_drop_down</i></li>
+        </ul>
+    </header>
+
+
+    <div class="menuTab">
+       
+
+       <a href="dashboard"><div class="pageTab">
+         <div class="tabing"><i class="material-icons">library_books</i><span>My Courses</span></div>
+       </div></a>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">person</i><span>My Profile</span></div>
+       </div>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">folder</i><span>Course Catalog</span></div>
+       </div>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">video_library</i><span>Videos</span></div>
+       </div>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">people</i><span>Forums</span></div>
+       </div>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">turned_in</i><span>My Certificates</span></div>
+       </div>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">language</i><span>Support Center</span></div>
+       </div>
+       <div class="pageTab">
+         <div class="tabing"><i class="material-icons">today</i><span>Learning Calendar</span></div>
+       </div>
+    </div>
